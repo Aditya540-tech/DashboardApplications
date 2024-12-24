63ebe5b43de2aab02f3392a8cf6563de7f6ed62b
Aditya540-tech <83633545+Aditya540-tech@users.noreply.github.com>
2024-12-24 05:53:27 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,13 @@ import ListingUserSchema from "./mongodb/models/ListingUserSchema.js";
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors(
+  {
+    origin:["https://deploy-mern-1whq.vercel.app"],
+    methods:["POST","GET"],
+    credentials:true
+  }
+));
 app.use(express.json());
 app.use(morgan("dev"));
 
